30ab68d87152b5c160d7c9fdc073fc271e1fa355
krrsalmane <karrounsalmane@gmail.com>
2025-02-28 16:56:34 +0000

    create my database

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,2 +1,11 @@
 create database doctorrv;
 
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       full_name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL,
+                       phone_number VARCHAR(20),
+                       specialization VARCHAR(100),
+                       role varchar (50),
+);
