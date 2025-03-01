f14a798a9481ea9a4b74ead39115acd77381388e
krrsalmane <karrounsalmane@gmail.com>
2025-03-01 22:19:15 +0000

    Register User

---
+++ src/main/java/com/doctorrv2/DAO/UserDAO.java
@@ -0,0 +1,26 @@
+package com.doctorrv2.DAO;
+
+import com.doctorrv2.Model.User;
+import java.sql.*;
+
+public class UserDAO {
+    private static final String INSERT_USER_SQL = "INSERT INTO users (full_name, email, password, phone, role, specialization) VALUES (?, ?, ?, ?, ?, ?)";
+    private static final String SELECT_USER_BY_EMAIL = "SELECT * FROM users WHERE email = ?";
+
+    public void registerUser(User user) throws SQLException {
+        try (Connection conn = DBconnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(INSERT_USER_SQL)) {
+            ps.setString(1, user.getFullName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+            ps.setString(4, user.getPhone());
+            ps.setString(5, user.getRole());
+            ps.setString(6, user.getSpecialization());
+            ps.executeUpdate();
+        }
+    }
+
+
+
+    }
+}
